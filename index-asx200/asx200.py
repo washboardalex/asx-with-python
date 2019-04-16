4caa89f062145745d379c9d9f9d948d1ca720e04
Aleksandar <aleksandar.gujas@gmail.com>
2019-04-16 13:07:49 +1000

    pulling data on stocks - must find api containing asx data

--- index-asx200/asx200.py
+++ index-asx200/asx200.py
@@ -1,7 +1,11 @@
 import bs4 as bs
+import datetime as dt
+import os
+import pandas_datareader.data as web
 import pickle
 import requests
 
+#Pull asx200 tickers from wikipedia
 def save_asx200_tickers():
     resp = requests.get('https://en.wikipedia.org/wiki/S%26P/ASX_200#Constituent_companies')
     soup = bs.BeautifulSoup(resp.text, 'lxml')
@@ -12,10 +16,34 @@ def save_asx200_tickers():
     for row in table.findAll('tr')[1:]:
         ticker = row.findAll('td')[0].text
         tickers.append(ticker)
+    
+    with open('asx200tickers.pickle','wb') as f:
+        pickle.dump(tickers, f)
 
     return tickers
 
-asx200_tickers = save_asx200_tickers()
+#get data for each ticker
+def get_asx_data(reload_asx = True):
+    if reload_asx:
+        tickers = save_asx200_tickers()
+    else:
+        with open("asc200tickers.pickle", "rb") as f:
+            tickers = pickle.load(f)
+    
+    if not os.path.exists('stock_dfs'):
+        os.makedirs('stock_dfs')
 
-print(asx200_tickers[201])
-print(len(asx200_tickers))
+    start = dt.datetime(2010, 1, 1)
+    end = dt.datetime.now()
+    for ticker in tickers:
+        # just in case your connection breaks, we'd like to save our progress!
+        if not os.path.exists('stock_dfs/{}.csv'.format(ticker)):
+            df = web.DataReader(ticker, 'quandl', start, end)
+            df.reset_index(inplace=True)
+            df.set_index("Date", inplace=True)
+            df = df.drop("Symbol", axis=1)
+            df.to_csv('stock_dfs/{}.csv'.format(ticker))
+        else:
+            print('Already have {}'.format(ticker))
+
+get_asx_data()
