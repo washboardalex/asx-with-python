b1b2e3e19fd40a84c35ce43e71b729325837b742
Aleksandar <aleksandar.gujas@gmail.com>
2019-04-17 13:07:16 +1000

    use pyasx lib for some asx data. For time series will work directly with asx JSON - possibly remove pyasx lib also once time series sorted out

--- index-asx200/asx200.py
+++ index-asx200/asx200.py
@@ -1,49 +1,60 @@
 import bs4 as bs
 import datetime as dt
 import os
+import pandas as pd
 import pandas_datareader.data as web
 import pickle
 import requests
+import time
+
+#=give pyasx library a try
+import pyasx.data.companies
+results = pyasx.data.companies.get_listed_companies()
+
+for result in results:
+    print (pyasx.data.companies.get_company_info(result['ticker']))
+
+
 
 #Pull asx200 tickers from wikipedia
-def save_asx200_tickers():
-    resp = requests.get('https://en.wikipedia.org/wiki/S%26P/ASX_200#Constituent_companies')
-    soup = bs.BeautifulSoup(resp.text, 'lxml')
+# def save_asx200_tickers():
+#     resp = requests.get('https://en.wikipedia.org/wiki/S%26P/ASX_200#Constituent_companies')
+#     soup = bs.BeautifulSoup(resp.text, 'lxml')
 
-    table = soup.table
+#     table = soup.table
 
-    tickers = []
-    for row in table.findAll('tr')[1:]:
-        ticker = row.findAll('td')[0].text
-        tickers.append(ticker)
+#     tickers = []
+#     for row in table.findAll('tr')[1:]:
+#         ticker = row.findAll('td')[0].text
+#         tickers.append(ticker)
     
-    with open('asx200tickers.pickle','wb') as f:
-        pickle.dump(tickers, f)
+#     with open('asx200tickers.pickle','wb') as f:
+#         pickle.dump(tickers, f)
 
-    return tickers
+#     return tickers
 
-#get data for each ticker
-def get_asx_data(reload_asx = True):
-    if reload_asx:
-        tickers = save_asx200_tickers()
-    else:
-        with open("asc200tickers.pickle", "rb") as f:
-            tickers = pickle.load(f)
+# #get data for each ticker
+# def get_asx_data(reload_asx = True):
+#     if reload_asx:
+#         tickers = save_asx200_tickers()
+#     else:
+#         with open("asc200tickers.pickle", "rb") as f:
+#             tickers = pickle.load(f)
     
-    if not os.path.exists('stock_dfs'):
-        os.makedirs('stock_dfs')
+#     if not os.path.exists('stock_dfs'):
+#         os.makedirs('stock_dfs')
 
-    start = dt.datetime(2010, 1, 1)
-    end = dt.datetime.now()
-    for ticker in tickers:
-        # just in case your connection breaks, we'd like to save our progress!
-        if not os.path.exists('stock_dfs/{}.csv'.format(ticker)):
-            df = web.DataReader(ticker, 'quandl', start, end)
-            df.reset_index(inplace=True)
-            df.set_index("Date", inplace=True)
-            df = df.drop("Symbol", axis=1)
-            df.to_csv('stock_dfs/{}.csv'.format(ticker))
-        else:
-            print('Already have {}'.format(ticker))
+#     start = dt.datetime(2010, 1, 1)
+#     end = dt.datetime.now()
+#     for ticker in tickers:
+#         # just in case your connection breaks, we'd like to save our progress!
+#         if not os.path.exists('stock_dfs/{}.csv'.format(ticker)):
+#             df = web.DataReader(ticker, 'robinhood', start, end)
+#             df.reset_index(inplace=True)
+#             df.set_index("Date", inplace=True)
+#             df = df.drop("Symbol", axis=1)
+#             df.to_csv('stock_dfs/{}.csv'.format(ticker))
+#         else:
+#             print('Already have {}'.format(ticker))
 
-get_asx_data()
+# get_asx_data()
