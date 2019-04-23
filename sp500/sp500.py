fd91228c2a3c64f0754f2324475b13c0d9a58382
Aleksandar <aleksandar.gujas@gmail.com>
2019-04-23 20:02:12 +1000

    collected stock price data

--- sp500/sp500.py
+++ sp500/sp500.py
@@ -12,19 +12,22 @@ import pandas as pd
 import requests
 import time
 
-# Pull asx200 tickers from wikipedia
-def save_asx200_tickers():
-    resp = requests.get('https://en.wikipedia.org/wiki/S%26P/ASX_200#Constituent_companies')
+# Pull sp500 tickers from wikipedia
+def save_sp500_tickers():
+    resp = requests.get('http://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
     soup = bs.BeautifulSoup(resp.text, 'lxml')
 
     table = soup.table
 
     tickers = []
+    print (table.findAll('tr')[1:])
     for row in table.findAll('tr')[1:]:
-        ticker = row.findAll('td')[0].text
+        ticker = row.findAll('td')[1].text
         tickers.append(ticker)
     
-    with open('asx200tickers.pickle','wb') as f:
+    print (tickers)
+    
+    with open('sp500tickers.pickle','wb') as f:
         pickle.dump(tickers, f)
 
     return tickers
@@ -32,23 +35,22 @@ def save_asx200_tickers():
 #get data for each ticker
 def get_asx_data(reload_asx = True):
     if reload_asx:
-        tickers = save_asx200_tickers()
+        tickers = save_sp500_tickers()
     else:
-        with open("asc200tickers.pickle", "rb") as f:
+        with open("sp500tickers.pickle", "rb") as f:
             tickers = pickle.load(f)
     
     if not os.path.exists('stock_dfs'):
         os.makedirs('stock_dfs')
 
-    start = dt.datetime(2010, 1, 1)
+    start = dt.datetime(2015, 1, 1)
     end = dt.datetime.now()
     for ticker in tickers:
         # just in case your connection breaks, we'd like to save our progress!
         if not os.path.exists('stock_dfs/{}.csv'.format(ticker)):
-            df = web.DataReader(ticker, 'robinhood', start, end)
+            df = web.DataReader(ticker, 'iex', start, end)
             df.reset_index(inplace=True)
-            df.set_index("Date", inplace=True)
-            df = df.drop("Symbol", axis=1)
+            df.set_index("date", inplace=True)
             df.to_csv('stock_dfs/{}.csv'.format(ticker))
         else:
             print('Already have {}'.format(ticker))
