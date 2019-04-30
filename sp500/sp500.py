7cd098b1b3418b5008125235927bd0a94246ec82
Aleksandar <aleksandar.gujas@gmail.com>
2019-04-30 22:38:24 +1000

    correlation table

--- sp500/sp500.py
+++ sp500/sp500.py
@@ -4,13 +4,12 @@ import os
 import pandas as pd
 import pandas_datareader.data as web
 import pickle
+import matplotlib.pyplot as plt
+from matplotlib import style
+import numpy as np 
 
-import requests
-import time
+style.use('dark_background')
 
-import pandas as pd
-import requests
-import time
 
 # Pull sp500 tickers from wikipedia
 def save_sp500_tickers():
@@ -73,9 +72,30 @@ def compile_data():
     print(main_df.head())
     main_df.to_csv('sp500_joined_closes.csv')
 
+def visualize_data():
+    df = pd.read_csv('sp500_joined_closes.csv')
+    df_corr = df.corr()
+    data1=df_corr.values
+    fig1=plt.figure()
+    ax1 = fig1.add_subplot(111)
+    heatmap1 = ax1.pcolor(data1, cmap = plt.cm.RdYlGn)
+    fig1.colorbar(heatmap1)
+    ax1.set_xticks(np.arange(data1.shape[1]) + 0.5, minor=False)
+    ax1.set_yticks(np.arange(data1.shape[0]) + 0.5, minor=False)
+    ax1.invert_yaxis()
+    ax1.xaxis.tick_top()
+    column_labels = df_corr.columns
+    row_labels = df_corr.index
+    ax1.set_xticklabels(column_labels)
+    ax1.set_yticklabels(row_labels)
+    plt.xticks(rotation=90)
+    heatmap1.set_clim(-1,1)
+    plt.tight_layout()
+    plt.savefig("correlations.png", dpi = (300))
+    plt.show()
+
+
 
 # get_sp_data()
-compile_data()
-
-
-
+#compile_data()
+visualize_data()
